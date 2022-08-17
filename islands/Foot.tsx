32bb89e6105979c2657ba1c6c15fc4ce06365165
moonstripe <kojinglick@gmail.com>
2022-08-16 17:57:13 -0700

    optimizations

--- islands/Foot.tsx
+++ islands/Foot.tsx
@@ -11,9 +11,9 @@ export default () => {
             <div class={tw`text-white fixed bottom-10 left-10 text-2xl hover:bg-black`} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
                 <p class={tw`text-white text-lg text-thin`}>contact:</p>
                 <div class={tw`flex flex-row `}>
-                    <p class={!isHover ? tw`p-2` : tw`p-2 align-middle rotate-45`}>
+                    <a class={!isHover ? tw`p-2` : tw`p-2 align-middle rotate-45`} onClick={isHover ? () => setHover(false) : () => setHover(true)}>
                         +
-                    </p>
+                    </a>
                     {
                         isHover ? <a href={`mailto:moonstripe@proton.me?subject=Let's Build Something Together&body=The sky's the limit!`} target="_blank" rel="noopener noreferrer" class={tw`mx-2 py-2`}>moonstripe@proton.me</a> : null
                     }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,7 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
+import { IS_BROWSER } from "fresh/runtime.ts"
 import { Handlers, PageProps } from "fresh/server.ts"
 import Layout from "../components/Layout.tsx";
 import Moon from "../islands/Moon.tsx";
@@ -24,12 +25,11 @@ export default function Home({ url }: PageProps) {
         <div id="splash" class={tw`z-1 flex flex-col`}>
           <div id="hero" class={tw`ml-10 flex flex-col`}>
             <h1 class={tw`text-4xl lg:text-8xl font-black`}>moonstripe</h1>
-            <i class={tw`font-extralight text-left pb-2`}>web development, out of this world.</i>
+            <i class={tw`font-extralight text-left`}>web development, out of this world.</i>
             {/* <button class={tw`rounded border-2 px-2 py-0 mr-auto`}>blast off</button> */}
           </div>
         </div>
-        <div class={tw`h-[35vh] lg:h-[40vh]`} />
-        <div id="features" class={tw`flex flex-col w-full`}>
+        <div id="features" class={tw`flex flex-col w-full absolute top-[100vh]`}>
           <ProductPages />
           <div class={tw`h-[35vh] lg:h-[25vh]`} />
           <DataVisualization />
